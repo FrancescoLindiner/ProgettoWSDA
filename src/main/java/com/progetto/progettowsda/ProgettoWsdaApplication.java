34c8f3d0b4e111c74504a1202d0f38e29f909857
FrancescoLindiner <fralindi01@gmail.com>
2024-03-17 10:00:01 +0100

    first commit

---
+++ src/main/java/com/progetto/progettowsda/ProgettoWsdaApplication.java
@@ -0,0 +1,13 @@
+package com.progetto.progettowsda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoWsdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgettoWsdaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/progetto/progettowsda/ProgettoWsdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progetto.progettowsda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoWsdaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
